7f9fe881718713d9bcfe34324d91578b9a0eb120
JCxYIS <jsn811@gmail.com>
2023-01-14 16:45:32 +0800

    Date and time

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const anchorageAspectRadio = 1023/702;
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+  const [date,setDate] = useState(new Date());  
+  const pad0 = (i)=>(i<10)?"0"+i:i;
+  const dateString = `${date.getFullYear()}/${pad0(date.getMonth()+1)}/${pad0(date.getDate())}`;
+  const timeString = `${pad0(date.getHours())}:${pad0(date.getMinutes())}:${pad0(date.getSeconds())}`;
+    
+  useEffect(() => {
+      var timer = setInterval(()=>setDate(new Date()), 1000)
+      window.addEventListener('resize', handleResize);
+      return function cleanup() {
+          clearInterval(timer)
+      }    
+  }, []);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+    console.log(width, height)
+  }
+
+  return (<>
+    <div id="bg"></div>
+    <img id="anchorage" src='/pic/AnchorageP.png' style={{left: (width-height*anchorageAspectRadio)/2}} />   
+    <div id="book" style={{left: (width-height*anchorageAspectRadio)/2+888}}>
+      <div id="date">{dateString}</div>
+      <div id="time">{timeString}</div>
+    </div> 
+  </>);
+}
+
+export default App;
